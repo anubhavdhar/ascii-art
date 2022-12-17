8962252a96dfacd14fe909156325cb58c1df22b7
Anubhav Dhar <anubhavdhar@kgpian.iitkgp.ac.in>
2022-12-17 14:47:17 +0530

    finer output, can rerun the same instance

--- AsciiArt.py
+++ AsciiArt.py
@@ -1,6 +1,6 @@
 ####### REQUIRED IMPORTS FOR TRANSFORMS #######
 
-from transforms import GrayScale, ReScale, ToAscii
+from transforms import GrayScale, ReScale, ToAscii, ToAsciiFiner
 from PIL import Image, ImageFont, ImageDraw, ImageFont, ImageOps
 import matplotlib.pyplot as plt
 import matplotlib.image as pltim
@@ -14,13 +14,20 @@ import os
 import webbrowser
 
 
+# Function for restarting code
+def Restart():
+	root.destroy()
+	cleanup()
+	AsciiArt()
+
 # Function for the filebrowser button
-def fileClick():
+def fileClick(ent):
 
 	#creating file dialog
 	file_type_options = (('All files', '*.*'), ('jpg files', '*.jpg'), ('png files', '*.png'))
 	file_path = filedialog.askopenfilename(title = 'Open the image file', initialdir = './pictures', filetypes = file_type_options)
-	e.insert(0, file_path)
+	ent.delete(0, END)
+	ent.insert(0, file_path)
 
 	#saving the image
 	im = pilimg.open(file_path)
@@ -29,15 +36,15 @@ def fileClick():
 
 
 # Will process the output when clicked.
-def process(clicked_size, output_text):
-
+def process(clicked_size, output_text, ent):
 
 	try:
 		# if some file is previously chosen	
 		im = pilimg.open('./temp_images/original_image.png')
 	except:
 		# if no file is chosen previously
-		print("Select a File")
+		ent.delete(0, END)
+		ent.insert(0, "Select a file first!")
 		return
 	
 
@@ -60,8 +67,16 @@ def process(clicked_size, output_text):
 	output_file_b.close()
 	output_file_w.close()
 
+
+	#creating ascii outputs of both white and black background
+	output_file_finer_b = open("output_file_finer_black_bg.txt","w")
+	output_file_finer_w = open("output_file_finer_white_bg.txt","w")
+	dimensions = ToAsciiFiner(im_r, output_file_finer_b, output_file_finer_w);
+	output_file_finer_b.close()
+	output_file_finer_w.close()
+
 	#displying the black background picture on the GUI
-	output_file = open("output_file_black_bg.txt","r")
+	output_file = open("output_file_finer_black_bg.txt","r")
 	output_text.grid_remove()
 	img_str = output_file.read()
 	scroll_bar = Scrollbar(root, orient = 'vertical')
@@ -79,37 +94,35 @@ def process(clicked_size, output_text):
 
 
 	# Positioning the output
-	output_text.grid(row = 1, column = 0, columnspan = 4)
+	output_text.grid(row = 1, column = 0, columnspan = 5)
 	output_file.close()
 
-	# Changing the 'Process' Button to 'Exit Program' Button
-	ExitButton = Button(root, text = "Exit Program", command = partial(exit))
-	ExitButton.grid(row = 0, column = 3)
+	# Changing the 'Process' Button to 'Try Again' Button
+	RedoButton = Button(root, text = "Try Again", command = partial(Restart))
+	RedoButton.grid(row = 0, column = 3)
 
 
-
-
-# Main function starts
-if __name__ == '__main__':
+def AsciiArt():
 
 	# Creating the root tkinter GUI
-	root = Tk();
+	global root
+	root = Tk()
 	root.title("Picture to Ascii converter | Python GUI | Anubhav Dhar")
 
 	# widget for output
 	output_text = Text(root)
 
 	# options for widths
-	options_size = ["Select Width", "20", "50", "80", "100", "150", "170", "200", "300", "350"]
+	options_size = ["Select Width", "20", "50", "80", "100", "150", "170", "200", "250", "300", "350"]
 	clicked_size = StringVar()
 	clicked_size.set(options_size[0])
 
 	# file dialogue entry
-	e = Entry(root, width=70)
-	e.grid(row=0, column=0)
+	ent = Entry(root, width=70)
+	ent.grid(row=0, column=0)
 	
 	# select file button
-	select_file_button = Button(root, text = "Browse file", command = partial(fileClick))
+	select_file_button = Button(root, text = "Browse file", command = partial(fileClick, ent))
 	select_file_button.grid(row = 0, column = 1)
 
 	# drop down button for choosing width
@@ -117,12 +130,24 @@ if __name__ == '__main__':
 	drop_down_button_size.grid(row = 0, column = 2)
 
 	# 'Process' Button
-	myButton = Button(root, text = "Process", command = partial(process, clicked_size, output_text))
+	myButton = Button(root, text = "Process", command = partial(process, clicked_size, output_text, ent))
 	myButton.grid(row = 0, column = 3)
 
-	# start the GUI
-	root.mainloop();
+	# 'Exit' Button
+	ExitButton = Button(root, text = "Exit", command = partial(exit))
+	ExitButton.grid(row = 0, column = 4)
 
+	# start the GUI
+	root.mainloop()
+	cleanup()
+
+def cleanup():
 	# removing temporary files
 	if os.path.exists('./temp_images/original_image.png'):
 		os.remove('./temp_images/original_image.png')
+
+
+# Main function starts
+if __name__ == '__main__':
+
+	AsciiArt();
--- transforms/toascii.py
+++ transforms/toascii.py
@@ -29,3 +29,59 @@ def ToAscii(image, output_file_b, output_file_w):
 
 	# returning the dimension
 	return (original_img.shape[0], original_img.shape[1] * 2);
+
+
+# outputs ascii art with both black and white intended backgrounds
+def ToAsciiFiner(image, output_file_b, output_file_w):
+	
+	"                         "
+	"........................."
+	":::::::::::::::::::::::::"
+	";;;;;;;;;;;;;;;;;;;;;;;;;"
+	"ccccccccccccccccccccccccc"
+	"CCCCCCCCCCCCCCCCCCCCCCCCC"
+	"3333333333333333333333333"
+	"0000000000000000000000000"
+	"BBBBBBBBBBBBBBBBBBBBBBBBB"
+	"#########################"
+	
+	original_img = np.copy(np.transpose(image, (0, 1)))
+
+	for i in range(original_img.shape[0]):
+		for j in range(original_img.shape[1]):
+			if original_img[i][j] <= 25:
+				output_file_w.write('##')
+				output_file_b.write('  ')
+			elif original_img[i][j] <= 50:
+				output_file_w.write('88')
+				output_file_b.write('..')
+			elif original_img[i][j] <= 76:
+				output_file_w.write('00')
+				output_file_b.write('::')
+			elif original_img[i][j] <= 101:
+				output_file_w.write('33')
+				output_file_b.write(';;')
+			elif original_img[i][j] <= 127:
+				output_file_w.write('CC')
+				output_file_b.write('cc')
+			elif original_img[i][j] <= 152:
+				output_file_w.write('cc')
+				output_file_b.write('CC')
+			elif original_img[i][j] <= 178:
+				output_file_w.write(';;')
+				output_file_b.write('33')
+			elif original_img[i][j] <= 203:
+				output_file_w.write('::')
+				output_file_b.write('00')
+			elif original_img[i][j] < 229:
+				output_file_w.write('..')
+				output_file_b.write('88')
+			else:
+				output_file_w.write('  ')
+				output_file_b.write('##')
+		
+		output_file_b.write('\n')
+		output_file_w.write('\n')
+
+	# returning the dimension
+	return (original_img.shape[0], original_img.shape[1] * 2);
